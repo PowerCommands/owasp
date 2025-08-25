a3cf249c7ae876d0c673973800e7eb807345a6d0
Harri Klingsten <harri.klingsten2@scb.se>
2025-08-25 16:32:09 +0200

    First working example

--- src/API1 2023 Broken Object Level Authorization/Api/Program.cs
+++ src/API1 2023 Broken Object Level Authorization/Api/Program.cs
@@ -1,9 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(policy =>
+	{
+		policy.WithOrigins("http://localhost:5002")
+			  .AllowAnyHeader()
+			  .AllowAnyMethod();
+	});
+});
 
 var app = builder.Build();
 
+app.UseCors();
 app.MapControllers();
 
 app.Run();
